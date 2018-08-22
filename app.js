8774bb5bad7db3c00f5a0c2e128eea59a08b8765
Chinthaka Hettiarachchi <chinthaka912@gmail.com>
2018-08-22 20:35:37 +0530

    creating a full API which does the four operations (CRUD)

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const bodyParser    = require('body-parser');
 const express       = require('express');
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://chino:123data@ds229722.mlab.com:29722/productstutorial';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
+
+app.use('/products', product);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 let port = 1234;
 
